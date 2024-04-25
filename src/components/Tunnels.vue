89e036152f4ffc3b671c186565b50b6054fddc3f
Darren Lau Wei Jie <darrenwjlau@gmail.com>
2024-04-25 19:52:24 +0800

    Update Tunnels.vue

--- src/components/Tunnels.vue
+++ src/components/Tunnels.vue
@@ -120,7 +120,7 @@
     },
     watch: {
     },
-    errorCaptured(error, vm, info) {
+    onErrorCaptured(error, vm, info) {
     console.log('errorCaptured', error, info)
     return false; // Prevents the error from propagating further
   },
